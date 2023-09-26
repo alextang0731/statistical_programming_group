ef0dd2460bbda5b7f0ebaa8e59052f42e7fa3ba1
SL Tang <alextang@SINGLIKs-MacBook-Air.local>
2023-09-26 14:26:35 +0100

    updated r file

---
+++ Project/project_1/Proj1.r
@@ -0,0 +1,9 @@
+a <- scan("./resource/4300-0.txt",what="character",skip=73,nlines=32858-73) 
+a <- gsub("_(","",a,fixed=TRUE) ## remove "_("
+split_punct = function(x){ #function to separate the punctuation marks
+  
+}
+
+unique = function(y){ #used to find the vector, b, of unique words in the Ulysses text, a
+  
+}
