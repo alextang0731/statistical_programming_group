125a5560f25640d24a847384b449962e761202e4
SL Tang <alextang@SINGLIKs-MacBook-Air.local>
2023-09-26 15:18:26 +0100

    updated file, and finished split_punct function

--- Project/project_1/Proj1.r
+++ Project/project_1/Proj1.r
@@ -1,7 +1,12 @@
 a <- scan("./resource/4300-0.txt",what="character",skip=73,nlines=32858-73) 
 a <- gsub("_(","",a,fixed=TRUE) ## remove "_("
 split_punct = function(x){ #function to separate the punctuation marks
-  
+  ii <- grep(",|\\.|;|!|:|\\?",x)
+  xs <- rep("",length(ii)+length(x))
+  iis <- ii+1:length(ii)
+  xs[iis] <- substr(x[ii],nchar(x[ii]),nchar(x[ii]))
+  xs[-iis] <- gsub(",|\\.|;|!|:|\\?","",x)
+  xs
 }
 
 unique = function(y){ #used to find the vector, b, of unique words in the Ulysses text, a
