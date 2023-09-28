df9c8dcf2e5ded79c9a6ce49c87950a1b92ec5a3
SL Tang <alextang@SINGLIKs-MacBook-Air.local>
2023-09-28 16:34:14 +0100

    tidy up the codee

--- Project/project_1/Proj1.r
+++ Project/project_1/Proj1.r
@@ -1,35 +1,48 @@
 a <- scan("./resource/4300-0.txt",what="character",skip=73,nlines=32858-73) 
 a <- gsub("_(","",a,fixed=TRUE) ## remove "_("
 
-split_punct <- function(x){ #function to separate the punctuation marks
-  ii <- grep(",|\\.|;|!|:|\\?",x) ##checking which string containing punctuation
-  xs <- rep("",length(ii)+length(x)) ##create a new empty vector
-  iis <- ii+1:length(ii) ## stated where should punctuation go in xs? 
-  xs[iis] <- substr(x[ii],nchar(x[ii]),nchar(x[ii])) ## insert the punctuation
-  xs[-iis] <- gsub(",|\\.|;|!|:|\\?","",x) ## insert the string without punctuation
+###function to separate the punctuation marks
+split_punct <- function(x){ 
+  ii <- grep(",|\\.|;|!|:|\\?",x)                    #checking which string containing punctuation
+  xs <- rep("",length(ii)+length(x))                 #create a new empty vector
+  iis <- ii+1:length(ii)                             #stated where should punctuation go in xs? 
+  xs[iis] <- substr(x[ii],nchar(x[ii]),nchar(x[ii])) #insert the punctuation
+  xs[-iis] <- gsub(",|\\.|;|!|:|\\?","",x)           #insert the string without punctuation
   return(xs)
   }
 
-new_a <- split_punct(a) #applying the split_punct function
-unique_words <- unique(tolower(new_a)) #unique words
-matching_index <- match(tolower(new_a), unique_words ) #finding the index of unique words
-count <- tabulate(matching_index) #counting the unique words
-m <- 500 #threshold number of occurrences
-b <- unique_words[count>m] #listing out the element that appear many times
-b
+#applying the split_punct function
+new_a <- split_punct(a) 
 
-#7 
-matching_index <- match(tolower(new_a), b) #finding the index of common words
+#unique words
+unique_words <- unique(tolower(new_a)) 
 
-matching_shifted_one <- c(matching_index[-1]) # Shift the index vector by one place
-matching_shifted_two <- c(matching_index[-(1:2)]) # Shift the index vector by one place
+#finding the index of unique words
+matching_index <- match(tolower(new_a), unique_words )
+
+#counting the unique words
+count <- tabulate(matching_index) 
+
+#threshold number of occurrences
+m <- 500
+
+#listing out the element that appear many times
+b <- unique_words[count>m] 
+
+#finding the index of common words
+matching_index <- match(tolower(new_a), b) 
+
+# Shift the index vector by one place
+matching_shifted_one <- c(matching_index[-1]) 
+
+# Shift the index vector by one place
+matching_shifted_two <- c(matching_index[-(1:2)]) 
 
 # Trim the last 2 rows/index
 matching_index = matching_index[1:length(matching_shifted_two)]
 
 # Trim the last 1 row/index
 matching_shifted_one = matching_shifted_one[1:length(matching_shifted_two)]
-
 
 # Create the T matrix
 T_matrix <- cbind(matching_index, matching_shifted_one, matching_shifted_two)
@@ -42,9 +55,6 @@ P_matrix <- cbind(matching_index, matching_shifted_one)
 
 # Identify common word pair (drop pairs that contain an NA)
 common_pairs<- P_matrix[rowSums(is.na(P_matrix)) == 0, ]
-
-
-##
 
 
 
