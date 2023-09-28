9b1a3d3e6f6596a0eee808076d938674a6dfbab2
AlimHanif <alim.kidh11@gmail.com>
2023-09-28 16:19:56 +0100

    add code to trim the vectors

--- Project/project_1/Proj1.r
+++ Project/project_1/Proj1.r
@@ -26,6 +26,13 @@ matching_index <- match(tolower(new_a), b) #finding the index of common words
 matching_shifted_one <- c(matching_index[-1]) # Shift the index vector by one place
 matching_shifted_two <- c(matching_index[-(1:2)]) # Shift the index vector by one place
 
+# Trim the last 2 rows/index
+matching_index = matching_index[1:length(matching_shifted_two)]
+
+# Trim the last 1 row/index
+matching_shifted_one = matching_shifted_one[1:length(matching_shifted_two)]
+
+
 # Create the T matrix
 T_matrix <- cbind(matching_index, matching_shifted_one, matching_shifted_two)
 
