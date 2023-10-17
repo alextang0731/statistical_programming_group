72e27fc7fac0f82670e81c6da46f5aeeaeb7a2db
AlimHanif <alim.kidh11@gmail.com>
2023-10-17 22:22:22 +0100

    add comments

--- Project/project_2/proj2_alim.r
+++ Project/project_2/proj2_alim.r
@@ -1,6 +1,11 @@
 library(ggplot2)
 
 slot_checker <- function(df_station) {
+    # Function to check the available slot in the station
+    # Parameters:
+    #   - df_station(data.frame): the statistics of the stations
+    # Return:
+    #   - availability(bool): Whether there is an available slot or not
     total_lower_capacity <- nrow(df_station[df_station$queue < df_station$max_queue, ])
     if (total_lower_capacity > 0) {
         availability <- TRUE
@@ -11,6 +16,11 @@ slot_checker <- function(df_station) {
 }
 
 add_a_new_car <- function(df_station) {
+    # Function to add a new car to the shorthest queue (if slot available)
+    # Parameters:
+    #   - df_station(data.frame): the statistics of the stations
+    # Return:
+    #   - df_station(data.frame): the statistics of the stations with new car if slot is available.
     short_queue <- min(df_station["queue"])
     short_stations <- df_station[df_station$queue == short_queue, ]
     short_stations <- df_station[df_station$queue == short_queue & df_station$queue < df_station$max_queue, ]
@@ -23,6 +33,13 @@ add_a_new_car <- function(df_station) {
 }
 
 process_a_car <- function(df_station, pmin, pmax) {
+    # Function to process a car (update the queue, subtract a car from the queue, assign a new car to be processed)
+    # Parameters:
+    #   - df_station(data.frame): the statistics of the stations
+    #   - pmin(int): the lower bound of processing time (alpha in the Uniform distribution)
+    #   - pmin(int): the upper bound of processing time (beta in the Uniform distribution)
+    # Return:
+    #   - df_station(data.frame): the statistics of the stations with updated state and queue
     df_station["next_car"] <- df_station$state <= df_station$next_state
 
     available_station <- df_station[df_station$next_car == TRUE & df_station$queue > 0, ]
@@ -41,10 +58,40 @@ process_a_car <- function(df_station, pmin, pmax) {
 }
 
 qsim <- function(mf = 5, mb = 5, a.rate = .1, trb = 40, trf = 40, tmb = 30, tmf = 30, maxb = 20) {
+    # Function to simulate the system
+    # Parameters:
+    #   - mf(int): Number of French stations
+    #   - mb(int): Number of British stations
+    #   - a.rate(float): is the probability of a car arriving each second.
+    #   - trb(int): upper bound of processing time in british stations (beta)
+    #   - trf(int): upper bound of processing time in french stations (beta)
+    #   - tmb(int): lower bound of processing time in british stations (alpha)
+    #   - tmf(int): lower bound of processing time in french stations (alpha)
+    #   - maxb(int):  the maximum british queue length (per station)
+    # Returns:
+    #   - nf(vector): a vector giving the average length of the french queues, for each simulation second
+    #   - nb(vector): a vector giving the average length of the british queues, for each simulation second
+    #   - eq(vector): a vector giving the average expected waiting time
+
     total_state <- 2 * 60 * 60
-    # total_state=2
-    france_station <- data.frame("station" = 1:mf, "queue" = rep(0, mf), "state" = total_state, "next_state" = total_state, "next_car" = TRUE, max_queue = 10^5, ptime = 0)
-    british_station <- data.frame("station" = 1:mf, "queue" = rep(0, mf), "state" = total_state, "next_state" = total_state, "next_car" = TRUE, max_queue = maxb, ptime = 0)
+    france_station <- data.frame(
+        "station" = 1:mf, 
+        "queue" = rep(0, mf), 
+        "state" = total_state, 
+        "next_state" = total_state, 
+        "next_car" = TRUE, 
+        max_queue = Inf, 
+        ptime = 0
+        )
+    british_station <- data.frame(
+        "station" = 1:mf, 
+        "queue" = rep(0, mf), 
+        "state" = total_state, 
+        "next_state" = total_state, 
+        "next_car" = TRUE, 
+        max_queue = maxb, 
+        ptime = 0
+        )
 
     nf <- c()
     nb <- c()
@@ -76,16 +123,18 @@ qsim <- function(mf = 5, mb = 5, a.rate = .1, trb = 40, trf = 40, tmb = 30, tmf 
         # Add a new car to British Station
         if (is_british_available & sum(france_station["queue"]) < n_state_f) {
             british_station <- add_a_new_car(british_station)
+            n_state_f <- sum(france_station["queue"])
         }
 
         british_station <- process_a_car(british_station, pmin = tmb, pmax = tmb + trb)
 
         # Statistic
-        mean_proc_time <- mean(france_station$ptime)
+        mean_proc_time_french <- mean(france_station$ptime)
+        mean_proc_time_british <- mean(british_station$ptime)
 
         nf <- c(nf, mean(france_station$queue))
         nb <- c(nb, mean(british_station$queue))
-        eq <- c(eq, n_state_f * mean_proc_time)
+        eq <- c(eq, (n_state_f * mean_proc_time_french) + (n_state_b * mean_proc_time_british))
 
         france_station["state"] <- france_station["state"] - 1
         british_station["state"] <- british_station["state"] - 1
@@ -109,6 +158,7 @@ df_stat2 <- data.frame(
     idx = 1:7200
 )
 
+# Todo: combine these plot into a single of graph (4 panels 2x2)
 ggplot(data = df_stat1, aes(x = idx, y = nf)) +
     geom_bar(stat = "identity", color = "blue")
 ggplot(data = df_stat1, aes(x = idx, y = nb)) +
