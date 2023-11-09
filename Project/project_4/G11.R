a773fdac252ee818870caf57ef6163d2bb42e583
SL Tang <alextang@SINGLIKs-MacBook-Air.local>
2023-11-09 16:37:13 +0000

    finished forward function

--- Project/project_4/G11.R
+++ Project/project_4/G11.R
@@ -2,9 +2,39 @@ netup <- function(d){
   h <- as.list(d)
   W <- list()
   b <- list()
-  for (i in 1 : length(d)){
-    W[[i]] <- runif(d[i], 0, 0.2)
-    b[[i]] <- runif(d[i], 0, 0.2)
+  for (i in 1 : (length(d)-1)){
+    W[[i]] <- runif(d[i+1], 0, 0.2)
+    b[[i]] <- runif(d[i+1], 0, 0.2)
   }
   list(h = h, W = W, b = b)
 }
+
+# ReLU function
+relu <- function(x) {
+  return(pmax(0, x))
+}
+
+out <- list()
+forward <- function(nn,inp){
+  output <- c()
+  out[[1]] <- inp
+  h <- nn$h
+  w <- nn$W
+  b <- nn$b
+  for (l in 1:(length(nn$h)-1)){
+    
+    #W^l_j * h^l
+    output <- unlist(lapply(w[[l]], function(W) sum(W * out[[l]])))
+    
+    #+b^l
+    out[[l+1]] <- relu(output + b[[l]])
+  }
+  
+  #SoftMax application
+  results <-  exp(out[[length(out)]])/sum(exp(unlist(out[[length(out)]])))
+  return(results)
+}
+
+re <- netup(c(4,8,7,3))
+f <- forward(re, c(1,2,3,4))
+
