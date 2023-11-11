cb11beb7df04d11721e9caba096c4ff11380d8e7
AlimHanif <alim.kidh11@gmail.com>
2023-11-11 15:14:56 +0000

    revise the W (netup) with a link matrix, set h as placeholder, optimize the forward pass with matrix multiplication

--- Project/project_4/G11.R
+++ Project/project_4/G11.R
@@ -1,59 +1,84 @@
-###Group Members:
-#Group 11
-#1. Alex Tang s2592944
-#2. Alim Hanif s2442474
-#3. Yuna Choi s2120762
+### Group Members:
+# Group 11
+# 1. Alex Tang s2592944
+# 2. Alim Hanif s2442474
+# 3. Yuna Choi s2120762
 
-###Contributions:
-#netup function : Alex
-#forward function: Yuna and Alex
-#backward and train function: Alim
+### Contributions:
+# netup function : Alex
+# forward function: Yuna and Alex
+# backward and train function: Alim
 
 
-#=======================================================#
-#Overview:
+# =======================================================#
+# Overview:
 
-#=======================================================#
+# =======================================================#
 
-#netup function
-netup <- function(d){
-  h <- as.list(d)
+# netup function
+netup <- function(d) {
+  # h <- as.list(d)
+  h <- list()
   W <- list()
   b <- list()
-  for (i in 1 : (length(d)-1)){
-    W[[i]] <- runif(d[i+1], 0, 0.2)
-    b[[i]] <- runif(d[i+1], 0, 0.2)
+  for (i in 1:(length(d) - 1)) {
+    h[[i]] <- rep(c(0), d[i + 1])
+    # W[[i]] <- runif(d[i+1], 0, 0.2)
+    W[[i]] <- matrix(runif(d[i] * d[i + 1], 0, 0.2), ncol = d[i + 1])
+    b[[i]] <- runif(d[i + 1], 0, 0.2)
   }
   list(h = h, W = W, b = b)
 }
 
+softmax <- function(X) {
+  return(exp(X) / sum(exp(X)))
+}
 
-#forward function
-forward <- function(nn,inp){
-  out <- list()
-  output <- c()
-  out[[1]] <- inp
+relu <- function(X) {
+  result <- pmax(0, X)
+  result <- matrix(result, ncol = ncol(X))
+  return(result)
+}
+
+# forward function
+forward <- function(nn, inp) {
   h <- nn$h
   w <- nn$W
   b <- nn$b
-  for (l in 1:(length(nn$h)-1)){
-    
-    #W^l_j * h^l
-    output <- unlist(lapply(w[[l]], function(W) sum(W * out[[l]])))
-    output <- output +  b[[l]]
-    if (l == (length(nn$h)-1)){
-      #SoftMax application
-      out[[l+1]] <-  exp(output)/sum(exp(output))
-    }else{
-      #apply ReLu function
-      out[[l+1]] <- pmax(0,(output))
+  h_prev <- matrix(unlist(iris[, 1:4]), ncol = 4)
+  for (l in 1:length(h)) {
+    if (l != length(h)) {
+      h[[l]] <- relu(h_prev %*% w[[l]]) + b[[l]]
+    } else {
+      h[[l]] <- softmax(h_prev %*% w[[l]]) + b[[l]]
     }
-    
-    
+    h_prev <- h[[l]]
   }
-  return(out)
+  return(h)
 }
 
 
+# forward <- function(nn,inp){
+#   out <- list()
+#   output <- c()
+#   out[[1]] <- inp
+#   h <- nn$h
+#   w <- nn$W
+#   b <- nn$b
+#   for (l in 1:(length(nn$h)-1)){
+
+#     #W^l_j * h^l
+#     output <- unlist(lapply(w[[l]], function(W) sum(W * out[[l]])))
+#     output <- output +  b[[l]]
+#     if (l == (length(nn$h)-1)){
+#       #SoftMax application
+#       out[[l+1]] <-  exp(output)/sum(exp(output))
+#     }else{
+#       #apply ReLu function
+#       out[[l+1]] <- pmax(0,(output))
+#     }
 
 
+#   }
+#   return(out)
+# }
