451badbf791c5d3dc3796d69a2aa3b6bd71962e0
alextang0731 <145350735+alextang0731@users.noreply.github.com>
2023-11-14 22:39:59 +0000

    fixed typo

--- Project/project_4/G11.R
+++ Project/project_4/G11.R
@@ -14,6 +14,7 @@
 # forward function: Yuna and Alex
 # backward and train function: Alim
 
+#Note: Everyone is contributed equally
 
 # =======================================================#
 # Overview:
@@ -21,7 +22,7 @@
 # descent, involves iteratively updating the network's weights and biases
 # based on the gradients of the loss function, computed using small batches
 # of training data. After that, it uses the "iris" dataset and trains a
-# 4-8-7-3 network as an example. The task is to predict the Species.
+# 4-8-7-3 network as an example and the task is to predict the Species.
 
 
 # Note: this code encode the labels into an integer [1,2,3]
@@ -35,29 +36,29 @@ softmax <- function(X) {
   # Arguments:
   #   - X: input vector/matrix
   # Return:
-  #   - X after softmax function has been implemented.
+  #   - X after Softmax function has been implemented.
   return(exp(X) / rowSums(exp(X)))
 }
 
 relu <- function(X) {
-  # Function to implement ReLu function: max(0, X)
+  # Function to implement ReLU function: max(0, X)
   # Arguments:
   #   - X: input vector/matrix
   # Return:
-  #   - X after ReLu function has been implemented.
-
+  #   - X after ReLU function has been implemented.
+  
   result <- pmax(0, X)
   result <- matrix(result, ncol = ncol(X))
   return(result)
 }
 
 relu_derivative <- function(x) {
-  # Function to calcuate the derivative of ReLu
+  # Function to calculate the derivative of ReLU
   # Arguments:
   #   - X: input vector/matrix
   # Return:
-  #   - X after derivative of ReLu function has been implemented.
-
+  #   - X after derivative of ReLU function has been implemented.
+  
   # when it is smaller than or equal to zero, it will become 0
   x[x <= 0] <- 0
   x[x > 0] <- 1
@@ -72,7 +73,7 @@ grad_update <- function(wb, grad, eta) {
   #   - eta: the step size.
   # Return:
   #   - return the updated parameters
-
+  
   return(wb - (eta * grad))
 }
 
@@ -87,12 +88,12 @@ netup <- function(d) {
   #     -> h: vector of length d[l] contains the nodes for layer l
   #     -> W: the weight matrix linking layer l to layer l+1
   #     -> b: the list of vector bias
-
+  
   # Setup placeholder for h, W, and n.
   h <- list()
   W <- list()
   b <- list()
-
+  
   # Assign the matrix and vector to the placeholder
   h[[1]] <- rep(c(0), d[1])
   for (i in 1:(length(d) - 1)) {
@@ -112,7 +113,7 @@ forward <- function(nn, inp) {
   #   - inp: vector of input values for the first layer
   # Return:
   #   - nn: updated NN with computed offset (h) from inp data.
-
+  
   h_prev <- inp
   nn$h[[1]] <- h_prev
   for (l in 1:length(nn$W)) {
@@ -123,7 +124,7 @@ forward <- function(nn, inp) {
       # apply SoftMax function to the output layer
       nn$h[[l + 1]] <- softmax((h_prev %*% nn$W[[l]] + nn$b[[l]]))
     }
-
+    
     h_prev <- nn$h[[l + 1]]
   }
   return(nn)
@@ -137,31 +138,31 @@ backward <- function(nn, k) {
   #   - k: output class k (label)
   # Return:
   #   - updated nn with gradient computation for w, b, and h
-
+  
   # number of weight and h
   n_weight <- length(nn$W)
   n_h <- length(nn$h)
   batch_size <- length(k)
-
+  
   # One hot encoding for k matrix (label)
   k_matrix <-
     matrix(0, nrow = nrow(nn$h[[n_h]]), ncol = ncol(nn$h[[n_h]]))
   k_matrix[cbind(1:length(k), k)] <- 1
-
+  
   # Setup a placeholder for the gradients
   dh <- list()
   dW <- list()
   db <- list()
-
+  
   # Get the Likelihood and subtract by 1 for true label
   d_L <- nn$h[[n_weight + 1]]
   d_L <- (d_L - k_matrix)
-
+  
   # Compute the gradient for the output layer
   dh[[n_weight + 1]] <- d_L
   dW[[n_weight]] <- ((t(nn$h[[n_weight]]) %*% d_L) / batch_size)
   db[[n_weight]] <- colMeans(d_L)
-
+  
   # Compute the gradient for the intermediate layers
   for (l in (n_weight):2) {
     # Compute the gradient of the offset, with relu derivative.
@@ -170,11 +171,11 @@ backward <- function(nn, k) {
     dW[[l - 1]] <- ((t(nn$h[[l - 1]]) %*% dh[[l]]) / batch_size)
     db[[l - 1]] <- colMeans(dh[[l]])
   }
-
+  
   nn$dh <- dh
   nn$dW <- dW
   nn$db <- db
-
+  
   return(nn)
 }
 
@@ -189,24 +190,24 @@ train <- function(nn, inp, k, eta = .01, mb = 10, nstep = 10000) {
   #   - nstep: number of step for training. (default: 10000)
   # Return:
   #   - nn: trained model from the input
-
+  
   n_miss_event <- 0 # for display
-
+  
   # Training loop for nsteps
   n_data <- nrow(inp)
   for (step in 1:nstep) {
     mb_indices <- sample(n_data, mb)
     X_train_mb <- inp[mb_indices, ]
     y_train_mb <- k[mb_indices]
-
+    
     # Forward and Backward propagration
     nn <- forward(nn, X_train_mb)
     nn <- backward(nn, y_train_mb)
-
+    
     # Update the parameters with computed gradient
     nn$W <- Map(grad_update, nn$W, nn$dW, eta)
     nn$b <- Map(grad_update, nn$b, nn$db, eta)
-
+    
     # Disclaimer!!
     # We wrote the code below to monitor the metrics during training.
     # We hard-coded it to be `FALSE`, because it is not required
@@ -214,14 +215,14 @@ train <- function(nn, inp, k, eta = .01, mb = 10, nstep = 10000) {
     # We understand that monitoring the metrics during
     # training is crucial. We can stop the training
     # when the model behaves unexpectedly.
-
+    
     is_display <- FALSE
     if (is_display) {
       output_layer_idx <- length(nn$h)
       y_pred <- nn$h[output_layer_idx][[1]]
       y_pred_int <- apply(y_pred, 1, which.max)
       miss_event <- sum(y_train_mb != y_pred_int)
-
+      
       n_miss_event <- n_miss_event + miss_event
       if (step %% 100 == 0) {
         miss_class <- n_miss_event / (step * mb)
